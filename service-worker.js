c5fa9cfceec9544846221baf392f643c0aaa1bc2
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2019-12-21 12:44:54 +0530

    Update packge json & Service worker

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-export function register(config) {
+export function register() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       const swUrl = `sw.js`;
-      registerValidSW(swUrl, config);
+      registerValidSW(swUrl);
     });
   }
 }
 
-function registerValidSW(swUrl, config) {
+function registerValidSW(swUrl) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
@@ -19,28 +19,9 @@ function registerValidSW(swUrl, config) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
-              // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
-              console.log(
-                'New content is available and will be used when all ' +
-                'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
-              );
-
-              // Execute callback
-              if (config && config.onUpdate) {
-                config.onUpdate(registration);
-              }
+              console.log('New content is available and will be used when page reloads & all tabs are closed');
             } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
               console.log('Content is cached for offline use.');
-
-              // Execute callback
-              if (config && config.onSuccess) {
-                config.onSuccess(registration);
-              }
             }
           }
         };
