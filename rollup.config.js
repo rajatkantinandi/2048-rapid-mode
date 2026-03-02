8db5de1374c99212bae963807f564a06a1077567
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2026-03-02 12:15:35 +0530

    fix errors

--- rollup.config.js
+++ rollup.config.js
@@ -16,12 +16,12 @@ module.exports = {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('bundle.css');
+			emitCss: false,
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production,
+				// inline component styles to avoid extra css bundling plugin
+				css: 'injected'
 			}
 		}),
 
